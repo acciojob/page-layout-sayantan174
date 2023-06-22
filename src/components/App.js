481417ba8ae680aaf8ee9358f551dfa9ca8a9ad8
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-22 22:09:33 +0530

    submitting code-Thu Jun 22 2023 22:09:32 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,12 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Welcome to my website</h1>
+        <p>This is the content of my website.</p>
+        <p>&copy; 2023 My Website. All rights reserved.</p>
     </div>
   )
 }
 
 export default App
+
